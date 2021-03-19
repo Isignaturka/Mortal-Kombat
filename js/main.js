e2f5b7b3f6570a4be8aeb5c892c287feeedb922e
Nikolay <45938905+Isignaturka@users.noreply.github.com>
2021-03-19 22:38:33 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#app').vide({
+      mp4: 'video/video.mpg4',
+      webm: 'video/video.webm',
+      ogv: 'video/video.ogv',
+      poster: 'image/cover-image.jpg'
+    });
+
+});
